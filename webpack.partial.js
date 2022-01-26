8c73f9326ffebf40e181542d5280e24d9c7ece36
MatasRut <m.rutkauskas@amberlo.io>
2022-01-26 17:49:31 +0200

    resolve issue

--- webpack.partial.js
+++ webpack.partial.js
@@ -5,13 +5,36 @@ module.exports = {
     rules: [
       {
         test: /\.s[ac]ss$/i,
+        exclude: /styles\.scss/i,
         use: [
           {
             loader: 'postcss-loader',
             options: {
-              postcssOptions: {plugins: [
-                postcssRTLCSS
-              ]}
+              postcssOptions: {
+                plugins: [
+                  postcssRTLCSS({
+                    ltrPrefix: ':host-context([dir=\'ltr\'])',
+                    rtlPrefix: ':host-context([dir=\'rtl\'])'
+                    }
+                  )
+                ]
+              }
+            }
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /styles\.scss/i,
+        use: [
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  postcssRTLCSS
+                ]
+              }
             }
           },
           'sass-loader',
